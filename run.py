5911b4181bcdd7f8ed4032cdbd18c17e645dc2d3
Ouail <ouail.bendidi@gmail.com>
2017-07-26 17:16:16 +0200

    add some comments

--- run.py
+++ run.py
@@ -14,7 +14,9 @@ FLAGS.gpu = 0.75 #how much of the GPU to use (between 0 and 1) 0 means use cpu
 FLAGS.track = True # wheither to activate tracking or not
 FLAGS.trackObj = "person" # the object to be tracked
 FLAGS.saveVideo = False  #whether to save the video or not
-FLAGS.BK_MOG = False # activate background substraction using MOG substraction,to help in worst case scenarion when YOLO cannor predict(able to detect mouvement, it's not ideal but well)
+FLAGS.BK_MOG = True # activate background substraction using cv2 MOG substraction,
+                        #to help in worst case scenarion when YOLO cannor predict(able to detect mouvement, it's not ideal but well)
+                        # helps only when number of detection < 5, as it is still better than no detection.
 FLAGS.skip = 0 # how many frames to skipp between each detection to speed up the network
 FLAGS.csv = False #whether to write csv file or not(only when tracking is set to True)
 FLAGS.display = True # display the tracking or not
