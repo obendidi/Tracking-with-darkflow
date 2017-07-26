33538cb9366036be25599e6b1862c8fbc964c995
Ouail <ouail.bendidi@gmail.com>
2017-07-26 16:37:36 +0200

    update

--- run.py
+++ run.py
@@ -10,13 +10,14 @@ FLAGS.demo = "test_2.avi" # video file to use, or if camera just put "camera"
 FLAGS.model = "darkflow/cfg/yolo.cfg" # tensorflow model
 FLAGS.load = "darkflow/bin/yolo.weights" # tensorflow weights
 FLAGS.threshold = 0.25 # threshold of decetion confidance (detection if confidance > threshold )
-FLAGS.gpu = 0.75 #how much of the GPU to use (between 0 and 1) 0 means use cpu , FPS on CPU (i5) : 0.5 /  FPS on GPU (8Go) : 15 (leave .3 for the tracker)
-FLAGS.track = False # wheither to activate tracking or not
+FLAGS.gpu = 0.75 #how much of the GPU to use (between 0 and 1) 0 means use cpu
+FLAGS.track = True # wheither to activate tracking or not
 FLAGS.trackObj = "person" # the object to be tracked
 FLAGS.saveVideo = False  #whether to save the video or not
-FLAGS.BK_MOG = False # activate background substraction using MOG substraction, if False we will be using YOLO , if TRue we will use BG
-FLAGS.skip = 1 # how many frames to skipp between each detection to speed up the network
+FLAGS.BK_MOG = False # activate background substraction using MOG substraction,to help in worst case scenarion when YOLO cannor predict(able to detect mouvement, it's not ideal but well)
+FLAGS.skip = 0 # how many frames to skipp between each detection to speed up the network
 FLAGS.csv = False #whether to write csv file or not(only when tracking is set to True)
+FLAGS.display = True # display the tracking or not
 
 tfnet = TFNet(FLAGS)
 
