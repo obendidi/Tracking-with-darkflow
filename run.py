b257ab2c83357259e282c6506e6afb46832682c2
Ouail <ouail.bendidi@gmail.com>
2017-08-10 12:32:15 +0200

    add sort as object tracker

--- run.py
+++ run.py
@@ -6,17 +6,18 @@ from darkflow.darkflow.net.build import TFNet
 FLAGS = argHandler()
 FLAGS.setDefaults()
 
-FLAGS.demo = "test_2.avi" # video file to use, or if camera just put "camera"
+FLAGS.demo = "test_1.avi" # video file to use, or if camera just put "camera"
 FLAGS.model = "darkflow/cfg/yolo.cfg" # tensorflow model
 FLAGS.load = "darkflow/bin/yolo.weights" # tensorflow weights
 FLAGS.threshold = 0.25 # threshold of decetion confidance (detection if confidance > threshold )
 FLAGS.gpu = 0.75 #how much of the GPU to use (between 0 and 1) 0 means use cpu
 FLAGS.track = True # wheither to activate tracking or not
-FLAGS.trackObj = "person" # the object to be tracked
+FLAGS.trackObj = "people" # the object to be tracked
 FLAGS.saveVideo = False  #whether to save the video or not
 FLAGS.BK_MOG = False # activate background substraction using cv2 MOG substraction,
                         #to help in worst case scenarion when YOLO cannor predict(able to detect mouvement, it's not ideal but well)
                         # helps only when number of detection < 5, as it is still better than no detection.
+FLAGS.tracker = "sort" # wich algorithm to use for tracking deep_sort/sort (NOTE : deep_sort only trained for people detection )
 FLAGS.skip = 0 # how many frames to skipp between each detection to speed up the network
 FLAGS.csv = False #whether to write csv file or not(only when tracking is set to True)
 FLAGS.display = True # display the tracking or not
