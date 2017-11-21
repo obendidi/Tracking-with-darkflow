84721c78e98270665d9445cbced3bcebb106604d
Ouail <ouail.bendidi@gmail.com>
2017-11-21 03:04:20 +0100

    forgotten to push fix

--- run.py
+++ run.py
@@ -7,21 +7,21 @@ FLAGS = argHandler()
 FLAGS.setDefaults()
 
 FLAGS.demo = "123.MOV" # video file to use, or if camera just put "camera"
-FLAGS.model = "darkflow/cfg/yolo.cfg" # tensorflow model
-FLAGS.load = "darkflow/bin/yolo.weights" # tensorflow weights
-# FLAGS.pbLoad = "tiny-yolo-voc-traffic.pb" # tensorflow model
-# FLAGS.metaLoad = "tiny-yolo-voc-traffic.meta" # tensorflow weights
+# FLAGS.model = "darkflow/cfg/yolo.cfg" # tensorflow model
+# FLAGS.load = "darkflow/bin/yolo.weights" # tensorflow weights
+FLAGS.pbLoad = "tiny-yolo-voc-traffic.pb" # tensorflow model
+FLAGS.metaLoad = "tiny-yolo-voc-traffic.meta" # tensorflow weights
 FLAGS.threshold = 0.7 # threshold of decetion confidance (detection if confidance > threshold )
 FLAGS.gpu = 0.8 #how much of the GPU to use (between 0 and 1) 0 means use cpu
 FLAGS.track = True # wheither to activate tracking or not
-# FLAGS.trackObj = ['Bicyclist','Pedestrian','Skateboarder','Cart','Car','Bus'] # the object to be tracked
-FLAGS.trackObj = ["person"]
-FLAGS.saveVideo = False  #whether to save the video or not
+FLAGS.trackObj = ['Bicyclist','Pedestrian','Skateboarder','Cart','Car','Bus'] # the object to be tracked
+#FLAGS.trackObj = ["person"]
+FLAGS.saveVideo = True  #whether to save the video or not
 FLAGS.BK_MOG = True # activate background substraction using cv2 MOG substraction,
                         #to help in worst case scenarion when YOLO cannor predict(able to detect mouvement, it's not ideal but well)
                         # helps only when number of detection < 3, as it is still better than no detection.
 FLAGS.tracker = "sort" # wich algorithm to use for tracking deep_sort/sort (NOTE : deep_sort only trained for people detection )
-FLAGS.skip = 3 # how many frames to skipp between each detection to speed up the network
+FLAGS.skip = 0 # how many frames to skipp between each detection to speed up the network
 FLAGS.csv = False #whether to write csv file or not(only when tracking is set to True)
 FLAGS.display = True # display the tracking or not
 
