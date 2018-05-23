f6d0799342489fdff1055e7c8d7ee36020af2110
Aleksey Artamonov <aleksartamonov@gmail.com>
2018-05-23 16:26:17 +0300

    tracking to saved video

--- run.py
+++ run.py
@@ -6,24 +6,26 @@ from darkflow.darkflow.net.build import TFNet
 FLAGS = argHandler()
 FLAGS.setDefaults()
 
-FLAGS.demo = "camera" # video file to use, or if camera just put "camera"
-FLAGS.model = "darkflow/cfg/yolo.cfg" # tensorflow model
-FLAGS.load = "darkflow/bin/yolo.weights" # tensorflow weights
+FLAGS.demo = "/home/aleksartamonov/yolo/darknet/examples/DJI_Phantom_Cow_Copter.mp4" # video file to use, or if camera just put "camera"
+FLAGS.model = "darkflow/cfg/yolov2.cfg" # tensorflow model
+FLAGS.load = "darkflow/bin/yolov2.weights" # tensorflow weights
 # FLAGS.pbLoad = "tiny-yolo-voc-traffic.pb" # tensorflow model
 # FLAGS.metaLoad = "tiny-yolo-voc-traffic.meta" # tensorflow weights
-FLAGS.threshold = 0.7 # threshold of decetion confidance (detection if confidance > threshold )
-FLAGS.gpu = 0.8 #how much of the GPU to use (between 0 and 1) 0 means use cpu
-FLAGS.track = False # wheither to activate tracking or not
-FLAGS.trackObj = ['Bicyclist', 'Pedestrian', 'Skateboarder', 'Cart', 'Car', 'Bus'] # the object to be tracked
-#FLAGS.trackObj = ["person"]
+FLAGS.threshold = 0.3 # threshold of decetion confidance (detection if confidance > threshold )
+FLAGS.gpu = 0.8 # how much of the GPU to use (between 0 and 1) 0 means use cpu
+FLAGS.gpuName = '/gpu:3'
+FLAGS.track = True # wheither to activate tracking or not
+FLAGS.trackObj = ['cow'] # the object to be tracked
 FLAGS.saveVideo = True  #whether to save the video or not
+FLAGS.savePath = 'output.avi'
 FLAGS.BK_MOG = True # activate background substraction using cv2 MOG substraction,
                         #to help in worst case scenarion when YOLO cannor predict(able to detect mouvement, it's not ideal but well)
                         # helps only when number of detection < 3, as it is still better than no detection.
 FLAGS.tracker = "sort" # wich algorithm to use for tracking deep_sort/sort (NOTE : deep_sort only trained for people detection )
-FLAGS.skip = 0 # how many frames to skipp between each detection to speed up the network
-FLAGS.csv = False #whether to write csv file or not(only when tracking is set to True)
-FLAGS.display = True # display the tracking or not
+FLAGS.skip = 3 # how many frames to skipp between each detection to speed up the network
+FLAGS.csv = True #whether to write csv file or not(only when tracking is set to True)
+FLAGS.csv_path = 'output.csv'
+FLAGS.display = False # display the tracking or not
 
 tfnet = TFNet(FLAGS)
 
